1445c0d90e2926e26ba9d98935f731d280ab4af3
Ryan Sanna <ryansann@umich.edu>
2020-09-21 15:56:57 -0700

    add types for key rotation action

---
+++ pkg/api/norman/customization/cluster/actions_key.go
@@ -0,0 +1,9 @@
+package cluster
+
+import (
+	"github.com/rancher/norman/types"
+)
+
+func (a ActionHandler) RotateEncryptionKey(actionName string, action *types.Action, apiContext *types.APIContext) error {
+	return nil
+}
